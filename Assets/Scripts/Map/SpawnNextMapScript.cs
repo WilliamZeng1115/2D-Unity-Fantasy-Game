fe00545a2a0d7bcf522e3e961a8ecde659917703
winson0228 <winson0228@gmail.com>
2018-06-16 21:11:58 -0700

    Edited sample Map 1 to have trigger that spawns the next map.

---
+++ Assets/Scripts/Map/SpawnNextMapScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNextMapScript : MonoBehaviour {
+	
+	public GameObject Map1;
+	float MapWidth;
+	float TriggerPosX;
+	// Use this for initialization
+	void Start () {
+		MapWidth = Map1.GetComponent<SpriteRenderer>().bounds.size.x;
+		TriggerPosX = transform.position.x;
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		//Destroy(other.gameObject);
+		Debug.Log ("!!!");
+		Debug.Log (MapWidth);
+		Debug.Log (TriggerPosX);
+		Debug.Log (MapWidth - TriggerPosX);
+		Debug.Log ((MapWidth - TriggerPosX) + TriggerPosX);
+		Vector2 triggerPos = this.transform.position;
+		Vector2 mapPos = transform.parent.position;
+		Vector2 nextMapPos =  triggerPos - mapPos;
+		Debug.Log (mapPos);
+		Debug.Log (nextMapPos);
+		Debug.Log (MapWidth + this.transform.parent.position.x);
+		//Instantiate(Map1, new Vector3((MapWidth - TriggerPosX) + TriggerPosX, transform.position.y, 0), Quaternion.identity);
+		Instantiate(Map1, new Vector2 (MapWidth + this.transform.parent.position.x, 0), Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
