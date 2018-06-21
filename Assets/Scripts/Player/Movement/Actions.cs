69fba55fe790149568839a8969fa2d7cc68b8563
William <zeng.william@gmail.com>
2018-06-20 17:46:44 -0700

    refactored actions script

--- Assets/Scripts/Player/Movement/Actions.cs
+++ Assets/Scripts/Player/Movement/Actions.cs
@@ -3,113 +3,129 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Actions : MonoBehaviour {
+  
+    // movement
+    private bool enableAutoMovement;
+    private bool direction; // false = left, true = right;
+    private bool enableFlying;
+    private bool isJumping;
 
-    private float swimForce = 500f;
-    public float xSpeed = 10.0f;
-    public float ySpeed = 10.0f;
-    public float padding = 1.0f;
-    float xmin;
+    private float jumpForce;
+    private float xSpeed;
+    private float ySpeed;
+    // max height
+    private float clampX;
+    private float clampY;
+
+    // keys
+    private KeyCode jumpKey;
+    private KeyCode directionKey;
+    private KeyCode autoMoveKey;
+    private KeyCode skillKey;
+    private KeyCode moveLeftKey;
+    private KeyCode moveRightKey;
+
+    // components
+    private BaseClass playerClass;
     private Rigidbody2D rigidBody2D;
-
-    public GameObject skillProjectile;
-
-    private KeyCode swimKey;
-    public bool enableMovement;
-    private int direction; //0 = left, 1 = right;
-    public bool flyingOn;
-    private bool isJump;
-
-    private BaseClass baseClass;
 
     // Use this for initialization
     void Start () {
-        float zdistance = transform.position.z - Camera.main.transform.position.z;
-        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, zdistance));
-        xmin = leftMost.x + padding;
-        rigidBody2D = GetComponent<Rigidbody2D>();
-        swimKey = KeyCode.Space;
-        enableMovement = true;
-        direction = 1;
-        flyingOn = false;
-        isJump = false;
-        baseClass = (BaseClass)GetComponent<BaseClass>();
+        // movement
+        initializeMovementVariables();
+
+        // keys
+        initializeKeys();
+
+        // components
+        initializeComponents();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(swimKey) && !isJump)
-        {
-            swimUp();
-            isJump = true;
-        }
-
         //If flying on some object, allowed to hold space to gain height
-        if (Input.GetKey(swimKey) && flyingOn) swimUp();
+        if (Input.GetKey(jumpKey)) moveVertical();
 
-        if (Input.GetKeyUp(KeyCode.Z)) enableMovement = !enableMovement;
+        if (Input.GetKeyUp(autoMoveKey)) enableAutoMovement = !enableAutoMovement;
            
-        if (Input.GetKeyUp(KeyCode.X))  reverseDirection();
+        if (Input.GetKeyUp(directionKey))  direction = !direction;
 
-        // if (Input.GetKeyUp(KeyCode.C)) skillShootLaser();
-
-        if (Input.GetKeyUp(KeyCode.V)) useSkill();
-
-        if (enableMovement)
+        if (Input.GetKeyDown(skillKey)) useSkill();
+        
+        if (enableAutoMovement)
         {
-            if (direction == 1)
-            {
-                Vector3 moveRight = Vector3.right * xSpeed * Time.deltaTime;
-                transform.position += moveRight;
-            }
-            else 
-                {
-                    Vector3 moveLeft = Vector3.left * xSpeed * Time.deltaTime;
-                    transform.position += moveLeft;
-                }
-            }   
-        else
+            var vectorDirection = direction ? Vector3.right : Vector3.left;
+            moveHorizontal(vectorDirection);
+        }
+        else if (Input.GetKey(moveRightKey))
         {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                Vector3 moveRight = Vector3.right * xSpeed * Time.deltaTime;
-                transform.position += moveRight;
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
-                {
-                    Vector3 moveLeft = Vector3.left * xSpeed * Time.deltaTime;
-                    transform.position += moveLeft;
-                }
-            }
-        //float newX = Mathf.Clamp(transform.position.x, xmin, float.MaxValue);
-        //transform.position = new Vector3(newX, transform.position.y, transform.position.z);
-    }
+            moveHorizontal(Vector3.right);
+        }
+        else if (Input.GetKey(moveLeftKey))
+        {
+            moveHorizontal(Vector3.left);
+        }
+        var clampPosition = transform.position;
+        clampPosition.y = Mathf.Clamp(clampPosition.y, -clampY, clampY);
+        transform.position = clampPosition;
+     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground") isJump = false;
+        if (other.gameObject.tag == "Ground") isJumping = false;
     }
 
-    private void swimUp()
+    // Initializers
+    private void initializeComponents()
     {
-        rigidBody2D.velocity = Vector2.zero;
-        rigidBody2D.AddForce(new Vector2(0, swimForce));
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        playerClass = (BaseClass)GetComponent<BaseClass>();
     }
 
-    //Make the character swap moving directions
-    private void reverseDirection()
+    private void initializeKeys()
     {
-        direction = 1 - direction;
-        Debug.Log(direction);
+        jumpKey = KeyCode.Space;
+        directionKey = KeyCode.X;
+        autoMoveKey = KeyCode.Z;
+        skillKey = KeyCode.V;
+        moveLeftKey = KeyCode.LeftArrow;
+        moveRightKey = KeyCode.RightArrow;
     }
 
-    //Use the equiped skill to damage enemies
-    //Spawn projectile or some martial arts skill
+    private void initializeMovementVariables()
+    {
+        enableAutoMovement = false;
+        direction = true;
+        enableFlying = true;
+        isJumping = false;
+
+        jumpForce = 500.0f;
+        xSpeed = 10.0f;
+        ySpeed = 10.0f;
+
+        clampX = 8;
+        clampY = 8;
+    }
+
+    // Helpers
+    private void moveHorizontal(Vector3 direction)
+    {
+        var displacement = direction * xSpeed * Time.deltaTime;
+        transform.position += displacement;
+    }
+
+    private void moveVertical()
+    {
+        if (enableFlying || !isJumping)
+        {
+            rigidBody2D.velocity = Vector2.zero;
+            rigidBody2D.AddForce(new Vector2(0, jumpForce));
+            isJumping = true;
+        }
+    }
+    
     private void useSkill()
     {
-        baseClass.basicAttack();
+        playerClass.basicAttack();
     }
-
-    // private void skillShootLaser() {
-    //    Instantiate(skillProjectile, transform.position + new Vector3(2f, 0, -0.001f), Quaternion.Euler(new Vector3(0, 0, -90)));
-    // }
 }
