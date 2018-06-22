d5b16cc80dc6bbe49473ad9266ce0ab9c617cfed
William <zeng.william@gmail.com>
2018-06-21 19:29:10 -0700

    refactored map generation

---
+++ Assets/Scripts/Map/Checkpoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    private MapManager mapManager;
+
+	// Use this for initialization
+	void Start () {
+        mapManager = GetComponentInParent(typeof(MapManager)) as MapManager;
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player")
+        {
+            if ((transform.position.x - other.transform.position.x) < 0)
+            {
+                mapManager.createNewMap(false); // create left
+            }
+            else if ((transform.position.x - other.transform.position.x) > 0)
+            {
+                mapManager.createNewMap(true); // create right
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MapManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+    public GameObject rightMap, leftMap;
+    private GameObject[] maps;
+
+    // temp for now... until we figure out how big a map we want so we can just set a default map width like
+    // private float mapWidth; as a global
+    private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        maps = Resources.LoadAll<GameObject>("Prefabs/Maps");
+
+        // temp for now
+        renderer = maps[0].GetComponent<SpriteRenderer>();
+    }
+
+    public void createNewMap(bool direction) // true is right, false is left
+    {
+        // delete the opposite direction map
+        deleteMap(!direction);
+        var currMap = direction ? leftMap : rightMap;
+
+        // temp with renderer...change it to global width later -> discuss
+        var mapWidth = direction ? renderer.bounds.size.x : -renderer.bounds.size.x;
+        var corner = currMap.transform.position.x;
+        var topCorner = corner + mapWidth;
+        var position = new Vector2(topCorner, currMap.transform.position.y);
+
+        var numOfMaps = maps.Length;
+        var index = Random.Range(0, numOfMaps - 1);
+
+        var newMap = Instantiate(maps[index], position, Quaternion.identity);
+        newMap.transform.parent = transform;
+        if (direction)
+        {
+            rightMap = newMap;
+        } else
+        {
+            leftMap = newMap;
+        }
+    }
+
+    private void deleteMap(bool direction) // true is right, false is left
+    {
+        if (!direction)
+        {
+            if (leftMap != null) Destroy(leftMap);
+            leftMap = rightMap;
+        }
+        if (direction)
+        {
+            if (rightMap != null) Destroy(rightMap);
+            rightMap = leftMap;
+        }
+    }
+}
