26af7c534d6fef80f0877865836653003d63899f
winson0228 <winson0228@gmail.com>
2018-06-26 22:02:34 -0700

    Added options pop up panel in game.

---
+++ Assets/Scripts/PanelScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelScript : MonoBehaviour {
+
+    public GameObject Panel;
+    private int counter = 0;
+
+    void Start ()
+    {
+        Panel.gameObject.SetActive(false);
+    }
+
+	public void showhidePanel()
+    {
+        counter++;
+        if (counter%2 == 1)
+        {
+            Panel.gameObject.SetActive(false);
+        } else
+        {
+            Panel.gameObject.SetActive(true);
+        }
+    }
+		
+}
