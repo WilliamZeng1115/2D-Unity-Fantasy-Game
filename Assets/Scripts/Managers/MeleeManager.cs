8d2b4e262d33079c2dd795453c65e84d58d1dda3
winson0228 <winson0228@gmail.com>
2018-08-28 00:44:22 -0700

    it will only apply attack speed on player's weapons.

--- Assets/Scripts/Managers/MeleeManager.cs
+++ Assets/Scripts/Managers/MeleeManager.cs
@@ -8,7 +8,7 @@ public class MeleeManager : WeaponManager {
     public override void attack()
     {
         base.attack();
-        //anim.SetFloat("AttackSpeed", stats["Dexterity"]);
+        if (transform.root.gameObject.tag == "Player") anim.SetFloat("AttackSpeed", stats["Dexterity"]);
         anim.Play(animationName);
     }
    
